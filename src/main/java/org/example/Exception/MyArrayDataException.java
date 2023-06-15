ef320cd3fe9c28ef5d2ebd0bb78fb084f9fd007d
Stepan <St3Ba4@gmail.com>
2023-06-15 22:22:18 +0400

    add description at Exception

--- src/main/java/org/example/Exception/MyArrayDataException.java
+++ src/main/java/org/example/Exception/MyArrayDataException.java
@@ -1,6 +1,13 @@
 package org.example.Exception;
 
 public class MyArrayDataException extends Exception {
+
+    /**
+     *
+     * @param i - first coordinate in 2D array
+     * @param j - first coordinate in 2D array
+     * @param value - value of cell
+     */
     public MyArrayDataException(int i, int j, String value) {
         super("Expected integer number at position [" + i + ", " + j
                 + "] , but received value \""+ value + "\".");
--- src/main/java/org/example/Exception/MyArraySizeException.java
+++ src/main/java/org/example/Exception/MyArraySizeException.java
@@ -1,11 +1,22 @@
 package org.example.Exception;
 
 public class MyArraySizeException extends Exception {
+    /**
+     * Exception thrown when the number of arrays of a two-dimensional array is incorrect
+     * @param errorSize - array size
+     * @param expectedSize - expected array size
+     */
     public MyArraySizeException(int errorSize, int expectedSize) {
         super("Column length is " + errorSize + " and not valid. Expected length "
                 + expectedSize + ".");
     }
 
+    /**
+     * Exception thrown when array @numberArray is wrongly sized
+     * @param numberArray - number array
+     * @param errorSize - array size
+     * @param expectedSize - expected array size
+     */
     public MyArraySizeException(int numberArray, int errorSize, int expectedSize) {
         super("In column " + numberArray + ". Length row " + errorSize
                 + " not valid. Expected length " + expectedSize + ".");
